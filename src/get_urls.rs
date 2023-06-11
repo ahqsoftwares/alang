d29db702254cbea33cf8120a29147840ae7a14c2
AHQ Miness <ahqsecret@gmail.com>
2023-06-11 23:27:29 +0530

    v0.0.2

---
+++ src/get_urls.rs
@@ -0,0 +1,104 @@
+use serde::{Deserialize, Serialize};
+
+use reqwest::blocking::Client;
+
+static UA: &str = "ALang/Installer";
+
+#[derive(Serialize, Deserialize)]
+struct Asset {
+    name: String,
+    browser_download_url: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ALangRelease {
+    tag_name: String,
+    assets: Vec<Asset>,
+}
+
+#[derive(Debug)]
+pub enum AssetClass {
+    WindowsTools,
+    LinuxTools,
+    MacosTools,
+    LinuxCli,
+    MacosCli,
+    WindowsCli,
+}
+
+#[derive(Debug)]
+pub struct ParsedAsset {
+    pub class: AssetClass,
+    pub url: String,
+}
+
+#[derive(Debug)]
+pub struct Urls {
+    pub version: String,
+    pub assets: Vec<ParsedAsset>,
+}
+
+impl Urls {
+    fn default() -> Self {
+        Self {
+            version: String::from("v0"),
+            assets: vec![],
+        }
+    }
+
+    fn set_version(&mut self, version: &String) {
+        self.version = version.clone();
+    }
+
+    fn push(&mut self, asset: ParsedAsset) {
+        self.assets.push(asset);
+    }
+}
+
+pub fn get_urls() -> Urls {
+    let client = Client::new();
+
+    let mut url = Urls::default();
+
+    let assets = client
+        .get("https://api.github.com/repos/ahqsoftwares/alang/releases/latest")
+        .header("User-Agent", UA.clone())
+        .send()
+        .unwrap()
+        .json::<ALangRelease>()
+        .unwrap();
+
+    url.set_version(&assets.tag_name.replace("lang-", ""));
+
+    assets.assets.iter().for_each(|asset| {
+        let asset = asset.clone().to_owned();
+
+        if &asset.name.contains("installer") == &false {
+            url.push(ParsedAsset {
+                class: (|| {
+                    let name = asset.name.clone();
+                    let name = name.as_str();
+
+                    if name == "tools-windows.zip" {
+                        AssetClass::WindowsTools
+                    } else if name == "tools-linux.zip" {
+                        AssetClass::LinuxTools
+                    } else if name == "tools-macos.zip" {
+                        AssetClass::MacosTools
+                    } else if name == "cli_linux" {
+                        AssetClass::LinuxCli
+                    } else if name == "cli_windows.exe" {
+                        AssetClass::WindowsCli
+                    } else if name == "cli_macos" {
+                        AssetClass::MacosCli
+                    } else {
+                        panic!("Unknown asset: {}", &asset.name);
+                    }
+                })(),
+                url: asset.browser_download_url.clone(),
+            });
+        }
+    });
+
+    url
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-use std::{fs, process, io::{ErrorKind, stdin, Read}};
+use std::{
+    fs,
+    io::{stdin, ErrorKind, Read},
+    process,
+};
 
 use chalk_rs::Chalk;
-use inquire::{Text, Confirm};
+use inquire::{Confirm, Text};
+
+mod download;
+mod get_urls;
+use download::*;
+use get_urls::*;
 
 fn main() {
-    let text = Text::new("Where should we install alang?")
-        .with_default((|| {
-            if cfg!(windows) {
-                "C:\\alang"
-            } else if cfg!(target_os = "linux") {
-                "/alang"
-            } else {
-                ""
-            }
-        })());
+    let ver = env!("CARGO_PKG_VERSION");
+    #[cfg(windows)]
+    let os = "Windows";
+
+    #[cfg(target_os = "linux")]
+    let os = "Linux";
+
+    #[cfg(target_os = "macos")]
+    let os = "Macos";
+
+    success(format!("ALang {} Installer v{}", &os, ver).as_str());
+
+    let text = Text::new("Where should we install alang?").with_default((|| {
+        if cfg!(windows) {
+            "C:\\alang"
+        } else if cfg!(target_os = "linux") {
+            "/alang"
+        } else {
+            "/alang"
+        }
+    })());
 
     let prompt = text.prompt();
 
@@ -27,55 +47,168 @@ fn main() {
             .prompt();
 
         if let Ok(true) = confirm {
-            info("> Downloading ALang cli..");
+            if let Ok(dir) = fs::read_dir(&data) {
+                if dir.count() != 0 {
+                    err("The selected directory contains files");
+                }
+            }
+
+            if let Err(_) = fs::create_dir_all(format!("{}/downloads", &data)) {
+                err(format!("Could not make directory at {}", &data).as_str());
+            }
+
+            info("> Getting latest release...");
+
+            let alang_release = get_urls();
+
+            info(format!("> Installing v{}", &alang_release.version).as_str());
+
+            info("> Downloading ALang cli for all platforms..");
+
+            alang_release
+                .assets
+                .iter()
+                .for_each(|asset| match &asset.class {
+                    AssetClass::WindowsCli => {
+                        info("  > Downloading ALang Cli for Windows");
+                        download(
+                            asset.url.clone(),
+                            format!("{}/downloads", &data),
+                            "cli_windows.exe".to_owned(),
+                            |current, total| {
+                                println!(
+                                    "      > {}% downloaded (Windows Cli)",
+                                    (current * 100) / total
+                                );
+                            },
+                        );
+                    }
+                    AssetClass::MacosCli => {
+                        info("  > Downloading ALang Cli for Macos");
+                        download(
+                            asset.url.clone(),
+                            format!("{}/downloads", &data),
+                            "cli_macos".to_owned(),
+                            |current, total| {
+                                println!(
+                                    "      > {}% downloaded (Macos Cli)",
+                                    (current * 100) / total
+                                );
+                            },
+                        );
+                    }
+                    AssetClass::LinuxCli => {
+                        info("  > Downloading ALang Cli for Linux");
+                        download(
+                            asset.url.clone(),
+                            format!("{}/downloads", &data),
+                            "cli_linux".to_owned(),
+                            |current, total| {
+                                println!(
+                                    "      > {}% downloaded (Linux Cli)",
+                                    (current * 100) / total
+                                );
+                            },
+                        );
+                    }
+                    _ => {}
+                });
 
             success("> ALang Cli Downloaded");
 
-            #[cfg(windows)]
-            let os = "Windows";
+            info("> Downloading tools for all platforms...");
 
-            #[cfg(target_os = "linux")]
-            let os = "Linux";
+            alang_release
+                .assets
+                .iter()
+                .for_each(|asset| match &asset.class {
+                    AssetClass::WindowsTools => {
+                        info("  > Downloading ALang Tools for Windows");
+                        download(
+                            asset.url.clone(),
+                            format!("{}/downloads", &data),
+                            "tools_windows.zip".to_owned(),
+                            |current, total| {
+                                println!(
+                                    "      > {}% downloaded (Windows)",
+                                    (current * 100) / total
+                                );
+                            },
+                        );
+                    }
+                    AssetClass::MacosTools => {
+                        info("  > Downloading ALang Tools for Macos");
+                        download(
+                            asset.url.clone(),
+                            format!("{}/downloads", &data),
+                            "tools_macos.zip".to_owned(),
+                            |current, total| {
+                                println!("      > {}% downloaded (Macos)", (current * 100) / total);
+                            },
+                        );
+                    }
+                    AssetClass::LinuxTools => {
+                        info("  > Downloading ALang Tools for Linux");
+                        download(
+                            asset.url.clone(),
+                            format!("{}/downloads", &data),
+                            "tools_linux.zip".to_owned(),
+                            |current, total| {
+                                println!("      > {}% downloaded (Linux)", (current * 100) / total);
+                            },
+                        );
+                    }
+                    _ => {}
+                });
+
+            success("> ALang Tools downloaded");
+
+            info("> Installing..");
+
+            if let Err(_) = fs::create_dir_all(format!("{}/tools", &data)) {
+                err("Could not create tools folder");
+            }
+
+            #[cfg(windows)]
+            let _to_extract = format!("{}/downloads/tools_windows.zip", &data);
 
             #[cfg(target_os = "macos")]
-            let os = "Macos";
+            let _to_extract = format!("{}/downloads/tools_macos.zip", &data);
 
-            info(
-                format!("> Downloading tools for {}", &os).as_str()
-            );
-
-            success(
-                format!("> ALang Tools for {os} downloaded").as_str()
-            );
-
-            info("> Extracting..");
+            #[cfg(target_os = "linux")]
+            let _to_extract = format!("{}/downloads/tools_linux.zip", &data);
 
             success("> ALang has been installed! 🎉");
 
             println!("-------------------------------------------------------");
 
-            println!(r#"░█████╗░██╗░░░░░░█████╗░███╗░░██╗░██████╗░
+            println!(
+                r#"░█████╗░██╗░░░░░░█████╗░███╗░░██╗░██████╗░
 ██╔══██╗██║░░░░░██╔══██╗████╗░██║██╔════╝░
 ███████║██║░░░░░███████║██╔██╗██║██║░░██╗░
 ██╔══██║██║░░░░░██╔══██║██║╚████║██║░░╚██╗
 ██║░░██║███████╗██║░░██║██║░╚███║╚██████╔╝
-╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░"#);
+╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░"#
+            );
 
             println!("-------------------------------------------------------");
 
             print!("> ALang Path: ");
-            
+
             let mut chalk = Chalk::new();
 
             chalk.yellow().bold().print(&data);
 
             print!(" <\n> ");
-            chalk.green().bold().print(&"Add the above install dir to your path variable");
+            chalk
+                .green()
+                .bold()
+                .print(&"Add the above install dir to your path variable");
             println!(" <");
 
             println!("-------------------------------------------------------");
 
-            info("Press any key to exit");
+            info("Press enter key to exit");
 
             let mut buf = [0; 1];
             let _ = stdin().read_exact(&mut buf);
@@ -91,20 +224,28 @@ fn main() {
 
 fn is_symlink(path: String) -> bool {
     match fs::symlink_metadata(path) {
-        Ok(metadata) => if metadata.file_type().is_symlink() {
-            return true
-        } else {
-            return false
+        Ok(metadata) => {
+            if metadata.file_type().is_symlink() {
+                return true;
+            } else {
+                return false;
+            }
         }
         Err(err) => {
             let err = err.kind();
 
             match err {
                 ErrorKind::NotFound => return false,
-                _ => return true
+                _ => return true,
             }
         }
     }
+}
+
+fn info(data: &str) {
+    let mut chalk = Chalk::new();
+
+    chalk.yellow().bold().println(&data);
 }
 
 fn err(data: &str) {
@@ -112,13 +253,12 @@ fn err(data: &str) {
 
     chalk.red().bold().println(&data);
 
+    info("Press enter key to exit");
+
+    let mut buf = [0; 1];
+    let _ = stdin().read_exact(&mut buf);
+
     process::exit(1);
-}
-
-fn info(data: &str) {
-    let mut chalk = Chalk::new();
-
-    chalk.yellow().bold().println(&data);
 }
 
 fn success(data: &str) {
