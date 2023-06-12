e7e1ce2b0cbb704c13fc3680487051ddacbede97
AHQ Miness <ahqsecret@gmail.com>
2023-06-12 20:49:16 +0530

    More

--- src/permission.rs
+++ src/permission.rs
@@ -1,7 +1,7 @@
 use std::{fs, process::Command};
 
 pub fn give_perms(path: String) {
-    let entries = fs::read_dir(path).unwrap();
+    let entries = fs::read_dir(&path).unwrap();
 
     entries.into_iter().for_each(|entry| {
         let entry = entry.unwrap();
@@ -9,12 +9,12 @@ pub fn give_perms(path: String) {
         let data = entry.metadata().unwrap();
 
         if data.is_dir() {
-            give_perms(format!("{}/{}", path, entry.file_name().to_str().unwrap()));
+            give_perms(format!("{}/{}", &path, entry.file_name().to_str().unwrap()));
         } else {
             Command::new("sudo")
                 .arg("chmod")
                 .arg("777")
-                .arg(format!("{}/{}", path, entry.file_name().to_str().unwrap()))
+                .arg(format!("{}/{}", &path, entry.file_name().to_str().unwrap()))
                 .spawn()
                 .unwrap()
                 .wait()
